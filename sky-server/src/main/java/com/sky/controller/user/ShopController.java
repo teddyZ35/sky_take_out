80d52dc34a1c314143f1e96eeed092dd6d4d235e
teddy <kaslanak0423@gmail.com>
2024-12-17 21:24:21 -0800

    shop status query and update

---
+++ sky-server/src/main/java/com/sky/controller/user/ShopController.java
@@ -0,0 +1,28 @@
+package com.sky.controller.user;
+
+import com.sky.result.Result;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.bind.annotation.*;
+
+@RestController("userShopController")
+@RequestMapping("/user/shop")
+@Api(tags = "shop interface")
+@Slf4j
+public class ShopController {
+
+    public static final String KEY = "SHOP_STATUS";
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    @GetMapping("/status")
+    @ApiOperation("get shop status")
+    public Result<Integer> getStatus() {
+        Integer shopStatus = (Integer) redisTemplate.opsForValue().get(KEY);
+        return Result.success(shopStatus);
+    }
+}
